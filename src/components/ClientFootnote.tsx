02fa4a69bc502ff199ef22eec2b58a1a01a25d09
Daniel Saewitz <git@saewitz.com>
2026-07-29 14:48:45 -0400

    Clean up footnote

--- src/components/ClientFootnote.tsx
+++ src/components/ClientFootnote.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useRef } from 'react';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 export interface Props {
@@ -10,21 +10,22 @@ export interface Props {
 }
 
 const Footnote = ({ index, content, children }: Props) => {
-  const [highlight, setHighlight] = useState(false);
   const ref = useRef<HTMLSpanElement>(null);
 
-  const onClick = async () => {
-    if (!ref.current) return;
-    setHighlight(true);
-    setTimeout(() => setHighlight(false), 2000);
+  const onClick = () => {
+    ref.current?.animate(
+      [
+        { textShadow: 'rgba(0,100,255,0.8) 1px 0 8px' },
+        { textShadow: 'rgba(0,100,255,0) 1px 0 4px' },
+      ],
+      { duration: 2000, easing: 'ease-out' }
+    );
   };
 
   return (
     <>
       <Tooltip placement="bottom">
-        <TooltipContent className="max-w-[240px] xl:hidden">
-          {content || children}
-        </TooltipContent>
+        <TooltipContent className="max-w-[240px] xl:hidden">{content || children}</TooltipContent>
         <TooltipTrigger>
           <sup
             className="footnote-index cursor-pointer text-xs"
@@ -36,14 +37,7 @@ const Footnote = ({ index, content, children }: Props) => {
         </TooltipTrigger>
       </Tooltip>
 
-      <span
-        className="footnote-content hidden h-0 pt-1 xl:flex"
-        ref={ref}
-        style={{
-          textShadow: highlight ? 'rgba(0,100,255, 0.8) 1px 0 8px' : undefined,
-          transition: 'text-shadow 0.3s ease-out',
-        }}
-      >
+      <span className="footnote-content hidden h-0 pt-1 xl:flex" ref={ref}>
         <sup className="footnote-index mt-0.5 text-xs">{index}</sup>
         &nbsp; <span className="text-sm">{content || children}</span>
       </span>
